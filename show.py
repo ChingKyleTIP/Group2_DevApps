97503859a75b77ea893e62922fa09d315ceff5b0
mejceledonio <158552386+mejceledonio@users.noreply.github.com>
2024-04-29 09:53:08 +0800

    Update show.py

--- show.py
+++ show.py
@@ -1,11 +1,8 @@
 import tkinter as tk
 import socket
-
 def get_current_ip():
-
     current_ip = socket.gethostbyname(socket.gethostname())
     ip_label.config(text="Current IPv4 address: " + current_ip)
-
 def get_ipv6_address():
     ipv6_addresses = [addrinfo[4][0] for addrinfo in socket.getaddrinfo(socket.gethostname(), None) if addrinfo[0] == socket.AF_INET6]
     if ipv6_addresses:
